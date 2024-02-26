eb511d01c7796980d5774983450cac03e0be6f10
egiros <148718730+egiros@users.noreply.github.com>
2024-02-26 18:01:11 +0100

    u

---
+++ U5-W1-D1/Controllers/DipendenteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace U5_W1_D1.Controllers
+{
+    public class DipendenteController : Controller
+    {
+        // GET: Dipendente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Dipendente/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Dipendente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dipendente/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dipendente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Dipendente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dipendente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Dipendente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ U5-W1-D1/Models/Pagamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace U5_W1_D1.Models
+{
+    public class Pagamento
+    {
+        public int PagamentoID { get; set; }
+        public int DipendenteID { get; set; }
+        public DateTime PeriodoPagamento { get; set; }
+        public decimal Ammontare { get; set; }
+        public string TipoPagamento { get; set; }
+    }
+}
